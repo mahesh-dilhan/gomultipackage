31a794fd523e1c0c2e6410bb1e8bfb87dd278346
mahesh <mail.mahesh.lk@gmail.com>
2021-10-11 21:05:18 +0800

    multi-package

--- embedinterace/main.go
+++ embedinterace/main.go
@@ -1,1 +1,23 @@
 package main
+
+type catalog interface {
+	shipping() float64
+	tax() float64
+}
+
+type discount interface {
+	offer() float64
+}
+
+type configurable struct {
+	name       string
+	price, qty float64
+}
+
+func (c *configurable) tax() float64 {
+	return c.price * c.qty * 0.05
+}
+
+func (c *configurable) shipping() float64 {
+	return c.qty * 5
+}
