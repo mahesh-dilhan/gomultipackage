97f7f8d6d1e91cec31bea294deb8afb686e77bab
mahesh <mail.mahesh.lk@gmail.com>
2021-10-12 21:07:01 +0800

    embded interface

--- embedinterace/main.go
+++ embedinterace/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type catalog interface {
 	shipping() float64
 	tax() float64
@@ -20,4 +22,56 @@ func (c *configurable) tax() float64 {
 
 func (c *configurable) shipping() float64 {
 	return c.qty * 5
+}
+
+func (c *configurable) offer() float64 {
+	return c.price * 0.15
+}
+
+type download struct {
+	name       string
+	price, qty float64
+}
+
+func (d *download) tax() float64 {
+	return d.price * d.qty * 0.10
+}
+
+type simple struct {
+	name       string
+	price, qty float64
+}
+
+func (s *simple) tax() float64 {
+	return s.price * s.qty * 0.03
+}
+
+func (s *simple) shipping() float64 {
+	return s.qty * 3
+}
+
+func (s *simple) offer() float64 {
+	return s.price * 0.10
+}
+
+func main() {
+	tshirt := configurable{}
+	tshirt.price = 250
+	tshirt.qty = 2
+	fmt.Println("Configurable Product")
+	fmt.Println("Shipping Charge: ", tshirt.shipping())
+	fmt.Println("Tax: ", tshirt.tax())
+	fmt.Println("Discount: ", tshirt.offer())
+
+	mobile := simple{"Samsung S-7", 3000, 2}
+	fmt.Println("\nSimple Product")
+	fmt.Println(mobile.name)
+	fmt.Println("Shipping Charge: ", mobile.shipping())
+	fmt.Println("Tax: ", mobile.tax())
+	fmt.Println("Discount: ", mobile.offer())
+
+	book := download{"Python in 24 Hours", 50, 1}
+	fmt.Println("\nDownloadable Product")
+	fmt.Println(book.name)
+	fmt.Println("Tax: ", book.tax())
 }
