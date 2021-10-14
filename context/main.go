810134abe3e2416ab646306dd37d4653a197f441
mahesh <mail.mahesh.lk@gmail.com>
2021-10-14 20:05:16 +0800

    embded interface

--- context/main.go
+++ context/main.go
@@ -11,3 +11,15 @@ func main() {
 	http.Handle("/welcome", inejctMsgID(helloWorldHandler))
 	http.ListenAndServe(":8080", nil)
 }
+
+//HelloWorld hellow world handler
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	msgID := ""
+	if m := r.Context().Value("msgId"); m != nil {
+		if value, ok := m.(string); ok {
+			msgID = value
+		}
+	}
+	w.Header().Add("msgId", msgID)
+	w.Write([]byte("Hello, world"))
+}
