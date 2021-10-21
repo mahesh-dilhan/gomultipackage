cb0f9b51bf91304eb5c719cf2482aae0721362fa
mahesh <mail.mahesh.lk@gmail.com>
2021-10-21 09:08:30 +0800

    protoc

--- simpleproto/main.go
+++ simpleproto/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/golang/protobuf/proto"
+)
+
 func main() {
+	elliot := &Person{
+		Name: "Elliot",
+		Age:  24,
+	}
+
+	data, err := proto.Marshal(elliot)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
 }
